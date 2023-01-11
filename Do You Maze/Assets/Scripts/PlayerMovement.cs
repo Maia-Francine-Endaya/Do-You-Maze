67c2422694172b9d0e0b1238c4df723e382460ad
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2023-01-11 10:31:54 +0800

    added_health_bar
    Added health bar to the player

--- Do You Maze/Assets/Scripts/PlayerMovement.cs
+++ Do You Maze/Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,39 @@ public class PlayerMovement : MonoBehaviour
   Rigidbody rb;
   [SerializeField] float movementSpeed = 6f;
 
+  public int maxHealth = 5;
+  public int currentHealth;
+
+  public HealthBar health;
+
     // Start is called before the first frame update
     void Start()
     {
-      rb = GetComponent<Rigidbody>();   
+      rb = GetComponent<Rigidbody>();
+      currentHealth = maxHealth;
+      health.SetMaxHealth(maxHealth);   
     }
 
     // Update is called once per frame
     void Update()
     {
+      //Player movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+      //Player health
+      //This is a test as there are no obstacles yet
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        TakeDamage(1);
+      }
+
+      void TakeDamage(int damage)
+      {
+        currentHealth -= damage;
+        health.SetHealth(currentHealth);
+      }
     }
 }
