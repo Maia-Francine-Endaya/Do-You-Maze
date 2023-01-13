8559694930aa16b75c6dc8a5813c4e859f0843e8
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2023-01-13 14:36:49 +0800

    added_funcitonalities_to_menus
    Added functionality to the menus in the game.

---
+++ Do You Maze/Assets/Scripts/WinGame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinGame : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other)
+  {
+    if(other.gameObject.tag == "Player")
+    {
+      SceneManager.LoadScene("WinMenu");
+    }
+  }
+}
